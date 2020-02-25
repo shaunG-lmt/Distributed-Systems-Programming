5ba4f5ac40f51a3d88e9c901a45cc19eebd5cf7a
556261 <556261@RB-203A-12>
2020-02-25 19:30:23 +0000

    Part 1 complete

---
+++ SignalRLabs/SignalRLabs/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRLabs.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task BroadcastMessage(string username, string message)
+        {
+            await Clients.All.SendAsync("GetMessage", username, message);
+        }
+    }
+}
