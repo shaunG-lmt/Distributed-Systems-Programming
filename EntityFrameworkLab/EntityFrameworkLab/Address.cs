1867890de760d7d2d073374c931fd6ca92918457
556261 <556261@RB-203A-12>
2020-03-05 16:02:52 +0000

    Application classes added

---
+++ EntityFrameworkLab/EntityFrameworkLab/Address.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkLab
+{
+    class Address
+    {
+        public string House_Name_or_Number { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string County { get; set; }
+        public string Postcode { get; set; }
+        public ICollection<Person> People { get; set; }
+        public Address() { }
+    }
+}
---
+++ EntityFrameworkLab/EntityFrameworkLab/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkLab
+{
+    class Person
+    {
+        public string First_Name { get; set; }
+        public string Middle_Name { get; set; }
+        public string Last_Name { get; set; }
+        public DateTime Date_of_Birth { get; set; }
+        public Address Address { get; set; }
+        public Person() { }
+    }
+}
