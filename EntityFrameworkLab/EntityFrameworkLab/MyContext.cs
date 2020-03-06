29b4c872498397843d1d5f77b8314103684dc0b2
556261 <556261@RB-203A-12>
2020-03-06 19:18:19 +0000

    Context classes added.

--- EntityFrameworkLab/EntityFrameworkLab/MyContext.cs
+++ EntityFrameworkLab/EntityFrameworkLab/MyContext.cs
@@ -7,5 +7,15 @@ namespace EntityFrameworkLab
 {
     class MyContext : DbContext
     {
+        public MyContext() : base() { }
+
+        public DbSet<Person> People { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = (localdb)\\mssqllocaldb; Database = EFLab;");
+            base.OnConfiguring(optionsBuilder);
+        }
     }
 }
--- EntityFrameworkLab/EntityFrameworkLab/Program.cs
+++ EntityFrameworkLab/EntityFrameworkLab/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace EntityFrameworkLab
 {
@@ -6,7 +7,30 @@ namespace EntityFrameworkLab
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var ctx = new MyContext())
+            {
+                Address addr = new Address()
+                {
+                    House_Name_or_Number = "64",
+                    Street = "Jeff Street",
+                    City = "England",
+                    Postcode = "JE21FF",
+
+                    People = new List<Person>()
+                };
+
+                Person prsn = new Person()
+                {
+                    First_Name = "Jeff",
+                    Middle_Name = "Is",
+                    Last_Name = "Name",
+                    Date_of_Birth = new DateTime(1960, 21, 12),
+                    Address = addr
+                };
+                ctx.Addresses.Add(addr);
+                ctx.People.Add(prsn);
+                ctx.SaveChanges();
+            }
         }
     }
 }
