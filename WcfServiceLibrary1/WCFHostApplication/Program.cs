25e11db6b3abbcf23fc4342cb82928616fbc2f28
shaunG-lmt <53610279+shaunG-lmt@users.noreply.github.com>
2020-03-17 17:53:01 +0000

    Host application made for service

---
+++ WcfServiceLibrary1/WCFHostApplication/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using WcfServiceLibrary1;
+
+namespace WCFHostApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host = new ServiceHost(typeof(TranslationService));
+
+            host.Open();
+
+            Console.WriteLine("Service Running.");
+            Console.ReadKey(true);
+
+            host.Close();
+        }
+    }
+}
