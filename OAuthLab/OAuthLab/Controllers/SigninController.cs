f1ecc361b21a3e6dd2e5b9626fa2cbedeac0e6d3
shaunG-lmt <53610279+shaunG-lmt@users.noreply.github.com>
2020-04-18 15:59:56 +0100

    exercise 2 "OAuthLab"

---
+++ OAuthLab/OAuthLab/Controllers/SigninController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+
+namespace OAuthLab.Controllers
+{
+    public class SigninController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return Challenge(new AuthenticationProperties()
+            {
+                RedirectUri = "/signin/index"
+            }, "Microsoft");
+        }
+    }
+}
---
+++ OAuthLab/OAuthLab/Startup.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+
+namespace OAuthLab
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddCookie("Bearer").AddOAuth("Microsoft", options =>
+            {
+                // Authentication details
+                options.ClientId = Configuration["Authentication:Microsoft:ClientId"];
+                options.ClientSecret = Configuration["Authentication:Microsoft:ClientSecret"];
+                options.CallbackPath = new PathString("/signin");
+
+                // Path for validation -> recieving authentication token -> recieving resource (OAUTH FLOW)
+                options.AuthorizationEndpoint = "https://login.microsoftonline.com/common/oauth2/v2.0/authorize";
+                options.TokenEndpoint = "https://login.microsoftonline.com/common/oauth2/v2.0/token";
+                options.UserInformationEndpoint = "https://graph.microsoft.com/v1.0/me";
+                options.Scope.Add("user.read");
+
+                // Get client identity (SHOULD be using HTTPS as someone else could use cookies to be the user)
+                options.ClaimActions.MapJsonKey(ClaimTypes.Name, "displayName");
+
+                // Sends request with JWT Auth JSON cookie and waits to recieve json object in return
+                options.Events = new OAuthEvents
+                {
+                    OnCreatingTicket = async context =>
+                    {
+                        var request = new HttpRequestMessage
+                        (HttpMethod.Get, context.Options.UserInformationEndpoint);
+
+                        request.Headers.Accept.Add(
+                        new MediaTypeWithQualityHeaderValue("application/json"));
+
+                        request.Headers.Authorization = new AuthenticationHeaderValue
+                        ("Bearer", context.AccessToken);
+
+                        var response = await context.Backchannel.SendAsync
+                        (request, HttpCompletionOption.ResponseHeadersRead, context.HttpContext.RequestAborted);
+
+                        var user = JObject.Parse(await response.Content.ReadAsStringAsync());
+
+                        context.RunClaimActions(user);
+                    }
+                };
+
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+            app.UseAuthentication();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
