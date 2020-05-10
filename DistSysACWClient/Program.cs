e10b8951efa390bff91e035c4356da3ab18ba684
shaunG-lmt <53610279+shaunG-lmt@users.noreply.github.com>
2020-05-10 16:10:35 +0100

    add Task 10 - Client - User - Get & Post

--- DistSysACWClient/Program.cs
+++ DistSysACWClient/Program.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
@@ -18,13 +19,18 @@ namespace DistSysACWClient
         static HttpClient client = new HttpClient();
         static void Main()
         {
+            // Setting the defaults for the client.
             client.BaseAddress = new Uri("https://localhost:44307/api/");
-
+            
+            // Startup user display and input.
             Console.WriteLine("Hello. What would you like to do?");
             string[] userInput = Console.ReadLine().Split(" ");
             Console.WriteLine("...please wait...");
+
+            // Request activation
             HandleRequest(userInput);
 
+            // Addtional requests, user display and input.
             while (true)
             {
                 Console.WriteLine("What would you like to do next?");
@@ -32,18 +38,29 @@ namespace DistSysACWClient
                 HandleRequest(userInput);
             }
         }
-        static async Task RunAsync(string requestUri)
+        static async Task RunAsync(string requestUri, string body, string apiKey, string httpMethod)
         {
             try
             {
-                Task<string> task = GetStringAsync(requestUri);
-                if (await Task.WhenAny(task, Task.Delay(20000)) == task)
+                switch (httpMethod)
                 {
-                    Console.WriteLine(task.Result);
-                }
-                else
-                {
-                    Console.WriteLine("Request Timed Out");
+                    // Get request with no apikey.
+                    case "get": 
+                        Task<string> taskGet = GetStringAsync(requestUri);
+                        if (await Task.WhenAny(taskGet, Task.Delay(20000)) == taskGet)
+                        { Console.WriteLine(taskGet.Result); }
+                        else
+                        { Console.WriteLine("Request Timed Out"); }
+                        break;
+
+                    // Post request with content in the body and no apikey.
+                    case "post": 
+                        Task<string> taskPost = PostStringAsync(requestUri, body);
+                        if (await Task.WhenAny(taskPost, Task.Delay(20000)) == taskPost)
+                        { Console.WriteLine(taskPost.Result); }
+                        else
+                        { Console.WriteLine("Request Timed Out"); }
+                        break;
                 }
             }
             catch (Exception e)
@@ -58,23 +75,56 @@ namespace DistSysACWClient
             responsestring = await response.Content.ReadAsStringAsync();
             return responsestring;
         }
+        static async Task<string> PostStringAsync(string path, string body)
+        {
+            //const string quote = "\"";
+            //body = quote + body + quote;
+
+            var jsonString = JsonConvert.SerializeObject(body);
+            var stringContent = new StringContent(jsonString, Encoding.UTF8, "application/json"); 
+            
+            
+            string responsestring = "";
+            
+            HttpResponseMessage response = await client.PostAsync(path, stringContent);
+            responsestring = await response.Content.ReadAsStringAsync();
+            return responsestring;
+        }
 
         static void HandleRequest(string[] request)
         {
             switch (request[0])
             {
-                case "TalkBack":
+                case "TalkBack": 
                     if (request[1] == "Hello")
                     {
-                        RunAsync("talkback/hello").Wait();
+                        RunAsync("talkback/hello", null, null, "get").Wait();
                     }
                     else if (request[1] == "Sort")
                     {
                         TalkBackSort(request);
                     }
-                    else
+                    break;
+
+                case "User":
+                    switch (request[1])
                     {
-                        // Invalid, handle.
+                        case "Get":
+                            RunAsync("user/new?username=" + request[2], null, null, "get").Wait();
+                            break;
+
+                        case "Post":
+                            RunAsync("user/new", request[2], null, "post").Wait();
+                            break;
+
+                        case "Set":
+                            break;
+
+                        case "Delete":
+                            break;
+
+                        case "Role":
+                            break;
                     }
                     break;
 
@@ -103,9 +153,9 @@ namespace DistSysACWClient
                 requestUri += "integers=" + numbers[i] +"&";
             }
             requestUri.TrimEnd(new char[] { '&' });
-            
+
             // Send request to server with URI.
-            RunAsync(requestUri).Wait();
+            RunAsync(requestUri, null, null, "get").Wait();
         }
     }
 }
