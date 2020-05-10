de77d10e6997342a529c5f35ad2662e1b29827fa
shaunG-lmt <53610279+shaunG-lmt@users.noreply.github.com>
2020-05-10 22:10:28 +0100

    fix Task 10 - Client User Set

--- DistSysACWClient/Program.cs
+++ DistSysACWClient/Program.cs
@@ -16,7 +16,9 @@ namespace DistSysACWClient
     #endregion
     class Program
     {
-        static HttpClient client = new HttpClient();
+        private static HttpClient client = new HttpClient();
+        private static string clientUsername;
+        private static string clientApiKey;
         static void Main()
         {
             // Setting the defaults for the client.
@@ -63,6 +65,13 @@ namespace DistSysACWClient
                         else
                         { Console.WriteLine("Request Timed Out"); }
                         break;
+                    case "del":
+                        Task<string> taskDel = DeleteAsync(requestUri, apiKey);
+                        if (await Task.WhenAny(taskDel, Task.Delay(20000)) == taskDel)
+                        { Console.WriteLine(taskDel.Result); }
+                        else
+                        { Console.WriteLine("Request Timed Out"); }
+                        break;
                 }
             }
             catch (Exception e)
@@ -79,32 +88,29 @@ namespace DistSysACWClient
         }
         static async Task<string> PostStringAsync(string path, string body)
         {
-            //const string quote = "\"";
-            //body = quote + body + quote;
-
             var jsonString = JsonConvert.SerializeObject(body);
             var stringContent = new StringContent(jsonString, Encoding.UTF8, "application/json"); 
-            
-            
             string responsestring = "";
-            
             HttpResponseMessage response = await client.PostAsync(path, stringContent);
             responsestring = await response.Content.ReadAsStringAsync();
             return responsestring;
         }
 
+
         static void HandleRequest(string[] request)
         {
             switch (request[0])
             {
-                case "TalkBack": 
-                    if (request[1] == "Hello")
-                    {
-                        RunAsync("talkback/hello", null, null, "get").Wait();
-                    }
-                    else if (request[1] == "Sort")
-                    {
-                        TalkBackSort(request);
+                case "TalkBack":
+                    { 
+                        if (request[1] == "Hello")
+                        {
+                            RunAsync("talkback/hello", null, null, "get").Wait();
+                        }
+                        else if (request[1] == "Sort")
+                        {
+                            TalkBackSort(request);
+                        }
                     }
                     break;
 
@@ -122,58 +128,14 @@ namespace DistSysACWClient
                                 break;
                             }
                         case "Set":
-                            // User display and control for saving user details locally.
                             {
-                                Console.WriteLine("To save user details a directory must be specified.");
-                                string directory;
-                                while (true)
-                                {
-                                    Console.WriteLine("Please enter the directory you wish to store user details at: ");
-                                    directory = Console.ReadLine();
-                                    Console.WriteLine("Are you sure \"" + directory + "\" is the correct. Please enter: [ y / n ] to confirm.");
-                                    string decision = Console.ReadLine();
-                                    if (decision == "y")
-                                    {
-                                        if (Directory.Exists(directory))
-                                        {
-                                            Console.WriteLine("User details will be stored at: " + directory + "/userdetails.txt");
-                                            // Getting user details from input.
-                                            try
-                                            {
-                                                directory += "/userdetails.txt";
-                                                string[] userDetails = { request[2], request[3] };
-                                                System.IO.File.WriteAllLines(directory, userDetails);
-                                                Console.WriteLine("Stored");
-                                                break;
-                                            }
-                                            catch
-                                            {
-                                                Console.WriteLine("Invalid format, please try again. Correct format: User Set username apikey");
-                                                Main();
-                                            }
-                                        }
-                                        else
-                                        {
-                                            Console.WriteLine("Directory does not exist... Please try again...");
-                                        }
-                                    }
-                                    else if (decision == "n")
-                                    {
-                                        // Loop resumes. User re-prompted.
-                                    }
-                                    else
-                                    {
-                                        Console.WriteLine("Invalid choice...");
-                                        // Loop resumes. User re-prompted.
-                                    }
-
-                                }
+                                clientUsername = request[2];
+                                clientApiKey = request[3];
+                                Console.WriteLine("Stored");
                             }
                             break;
-
                         case "Delete":
                             break;
-
                         case "Role":
                             break;
                     }
