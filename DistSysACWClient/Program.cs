3b8c3c4f0f3932a5fe4f1e0fe73b8bd70bacfed9
shaunG-lmt <53610279+shaunG-lmt@users.noreply.github.com>
2020-05-10 08:42:18 +0100

    add Task 10 - Client Talkback

--- DistSysACWClient/Program.cs
+++ DistSysACWClient/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -11,11 +12,100 @@ using Newtonsoft.Json;
 namespace DistSysACWClient
 {
     #region Task 10 and beyond
-    class Client
+    #endregion
+    class Program
     {
-        static void Main(string[] args)
+        static HttpClient client = new HttpClient();
+        static void Main()
         {
+            client.BaseAddress = new Uri("https://localhost:44307/api/");
+
+            Console.WriteLine("Hello. What would you like to do?");
+            string[] userInput = Console.ReadLine().Split(" ");
+            Console.WriteLine("...please wait...");
+            HandleRequest(userInput);
+
+            while (true)
+            {
+                Console.WriteLine("What would you like to do next?");
+                userInput = Console.ReadLine().Split(" ");
+                HandleRequest(userInput);
+            }
+        }
+        static async Task RunAsync(string requestUri)
+        {
+            try
+            {
+                Task<string> task = GetStringAsync(requestUri);
+                if (await Task.WhenAny(task, Task.Delay(20000)) == task)
+                {
+                    Console.WriteLine(task.Result);
+                }
+                else
+                {
+                    Console.WriteLine("Request Timed Out");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.GetBaseException().Message);
+            }
+        }
+        static async Task<string> GetStringAsync(string path)
+        {
+            string responsestring = "";
+            HttpResponseMessage response = await client.GetAsync(path);
+            responsestring = await response.Content.ReadAsStringAsync();
+            return responsestring;
+        }
+
+        static void HandleRequest(string[] request)
+        {
+            switch (request[0])
+            {
+                case "TalkBack":
+                    if (request[1] == "Hello")
+                    {
+                        RunAsync("talkback/hello").Wait();
+                    }
+                    else if (request[1] == "Sort")
+                    {
+                        TalkBackSort(request);
+                    }
+                    else
+                    {
+                        // Invalid, handle.
+                    }
+                    break;
+
+                case "Exit":
+                    Environment.Exit(0);
+                    break;
+
+                default:
+                    // Invalid, handle.
+                    break;
+            }
+        }
+
+        static void TalkBackSort(string[] request)
+        {
+            // Get numbers from user input.
+            char[] ch = { '[',']' };
+            string[] numbers = request[2].Split(",");
+            numbers[0] = numbers[0].TrimStart(ch);
+            numbers[numbers.Length-1] = numbers[numbers.Length-1].TrimEnd(ch);
+            
+            // Build request URI.
+            string requestUri = "talkback/sort?";
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                requestUri += "integers=" + numbers[i] +"&";
+            }
+            requestUri.TrimEnd(new char[] { '&' });
+            
+            // Send request to server with URI.
+            RunAsync(requestUri).Wait();
         }
     }
-    #endregion
 }
