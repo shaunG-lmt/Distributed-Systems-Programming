729ed6115167b7ff9a88438bf46289e22f2e5103
shaunG-lmt <53610279+shaunG-lmt@users.noreply.github.com>
2020-05-14 17:35:17 +0100

    fix constructor for archive

---
+++ DistSysACW/Models/Log_Archive.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DistSysACW.Models
+{
+    public class Log_Archive
+    {
+        public Log_Archive() { }
+
+        public Log log { get; set; }
+    }
+}
